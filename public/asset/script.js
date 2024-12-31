76090e272a1c288b92fd2a5e4e458d09de89d62e
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2024-12-31 10:27:07 +0700

    Update script.js

--- public/asset/script.js
+++ public/asset/script.js
@@ -1,1 +1,15 @@
-y
+fetch('https://api.medanpedia.co.id/profile')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    // tampilkan data di halaman
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+    alert('Error fetching data from API');
+  });
